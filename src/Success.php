18d48551c5883d36242b154cd48d281b53ac8645
Graham Campbell <hello@gjcampbell.co.uk>
2026-08-24 02:14:58 +0100

    Make Result covariant in its value and error types

--- src/Success.php
+++ src/Success.php
@@ -17,10 +17,9 @@ use PhpOption\None;
 use PhpOption\Some;
 
 /**
- * @template T
- * @template E
+ * @template-covariant T
  *
- * @extends \GrahamCampbell\ResultType\Result<T,E>
+ * @extends \GrahamCampbell\ResultType\Result<T,never>
  */
 final class Success extends Result
 {
@@ -48,7 +47,7 @@ final class Success extends Result
      *
      * @param S $value
      *
-     * @return \GrahamCampbell\ResultType\Result<S,E>
+     * @return \GrahamCampbell\ResultType\Success<S>
      */
     public static function create($value)
     {
@@ -72,7 +71,7 @@ final class Success extends Result
      *
      * @param callable(T):S $f
      *
-     * @return \GrahamCampbell\ResultType\Result<S,E>
+     * @return \GrahamCampbell\ResultType\Success<S>
      */
     public function map(callable $f)
     {
@@ -97,7 +96,7 @@ final class Success extends Result
     /**
      * Get the error option value.
      *
-     * @return \PhpOption\Option<E>
+     * @return \PhpOption\Option<never>
      */
     public function error()
     {
@@ -109,9 +108,9 @@ final class Success extends Result
      *
      * @template F
      *
-     * @param callable(E):F $f
+     * @param callable(never):F $f
      *
-     * @return \GrahamCampbell\ResultType\Result<T,F>
+     * @return \GrahamCampbell\ResultType\Success<T>
      */
     public function mapError(callable $f)
     {
